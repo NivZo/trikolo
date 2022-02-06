a43cec95d646409908016fb04116e682f77f89bc
Niv Zohar <t-nivzohar@microsoft.com>
2022-02-06 23:01:30 +0200

    added older icons just in case

--- src/gameScreen.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import GameScreen from './screens/gameScreen/gameScreen';
-
-test('renders learn react link', () => {
-  render(<GameScreen />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
